28ea3a80f6d25cc28a49ec436a80d352ab8a96b5
Askar Sariev <askar-64@mail.ru>
2021-06-21 21:48:28 +0300

    Add Factory Method

---
+++ src/net/proselyte/basepatterns/creational/factory/Program.java
@@ -0,0 +1,18 @@
+package net.proselyte.basepatterns.creational.factory;
+
+public class Program {
+    public static void main(String[] args) {
+        DeveloperFactory developerFactory = createDeveloperBySpecialty("java");
+        Developer developer = developerFactory.createDeveloper();
+        developer.writeCode();
+    }
+    static DeveloperFactory createDeveloperBySpecialty(String specialty) {
+        if (specialty.equalsIgnoreCase("java")) {
+            return new JavaDeveloperFactory();
+        } else if (specialty.equalsIgnoreCase("c++")) {
+            return new CppDeveloperFactory();
+        } else {
+            throw new RuntimeException(specialty + " is unknown specialty");
+        }
+    }
+}
