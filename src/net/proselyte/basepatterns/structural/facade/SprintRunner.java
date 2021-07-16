e516bf02055b07ef01e92b6944e03592f0e9aba7
Askar Sariev <askar-64@mail.ru>
2021-07-16 19:07:36 +0300

    Add Facade

---
+++ src/net/proselyte/basepatterns/structural/facade/SprintRunner.java
@@ -0,0 +1,8 @@
+package net.proselyte.basepatterns.structural.facade;
+
+public class SprintRunner {
+    public static void main(String[] args) {
+        Workflow workflow = new Workflow();
+        workflow.solveProblem();
+    }
+}
