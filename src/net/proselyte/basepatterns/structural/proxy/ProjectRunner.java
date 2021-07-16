d72b87110f7a948cdda5e832fafe3d57ff8a5bda
Askar Sariev <askar-64@mail.ru>
2021-07-16 20:07:47 +0300

    Add Flyweight, Proxy

---
+++ src/net/proselyte/basepatterns/structural/proxy/ProjectRunner.java
@@ -0,0 +1,9 @@
+package net.proselyte.basepatterns.structural.proxy;
+
+public class ProjectRunner {
+    public static void main(String[] args) {
+        Project project = new ProxyProject("http://www.github.com/................");
+
+        project.run();
+    }
+}
