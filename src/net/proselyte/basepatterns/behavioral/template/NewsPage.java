abd3a61becffb5b37ef6fe495300799c05dcd5bf
Askar Sariev <askar-64@mail.ru>
2021-07-17 14:10:59 +0300

    Add Template method

---
+++ src/net/proselyte/basepatterns/behavioral/template/NewsPage.java
@@ -0,0 +1,9 @@
+package net.proselyte.basepatterns.behavioral.template;
+
+public class NewsPage extends WebsiteTemplate {
+
+    @Override
+    public void showPageContent() {
+        System.out.println("News");
+    }
+}
